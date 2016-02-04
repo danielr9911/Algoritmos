7b2cedf03c778045631d9c68ed5e7b5ae16aa556
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2016-02-04 11:45:34 -0500

    Documentación del Diagraph AM

--- Lab1/DigraphAM.java
+++ Lab1/DigraphAM.java
@@ -1,50 +1,62 @@
 import java.util.ArrayList;
+
 /**
- * Esta clase es una implementación de un digrafo usando matrices de adyacencia
- * 
- * @author Mauricio Toro 
- * @author  Daniel Rendón Montaño
- * @author  Juan Daniel Morales Arias
- * @author  Cristyam Sepulveda Vasquez
+ * Esta clase es una implementacion de un digrafo usando matrices de adyacencia
+ *
+ * @author Mauricio Toro
+ * @author Daniel Rendon Montano
+ * @author Juan Daniel Morales Arias
+ * @author Cristyam Sepulveda Vasquez
  * @version 04/02/2016
  */
-public class DigraphAM extends Graph
-{
-	private int [][] mat;
-	private ArrayList<Integer> successors;
-   
-   /**
-    * Constructor de la clase DiagphAM
-    */
-	public DigraphAM(int size)
-   {
-	   super(size);
-       mat = new int [size][size];   
-   }
-   
-   /**
-    * 
-    */
-   public int getWeight(int source, int destination)
-   {
-	   return mat[source][destination];
+public class DigraphAM extends Graph {
+    private int[][] mat;
+    private ArrayList<Integer> successors;
+
+    /**
+     * Constructor de la clase DiagphAM
+     */
+    public DigraphAM(int size) {
+        super(size);
+        mat = new int[size][size];
     }
-   
-   public void addArc(int source, int destination, int weight)
-   {
-	   mat[source][destination] = weight;
-   }
-  
-   public ArrayList<Integer> getSuccessors(int vertex)
-   {
-	   successors = new ArrayList<Integer>();
-	   for(int i=0;i<size();i++){
-		   if(mat[vertex][i]!=0){
-			   successors.add(i);
-			   
-		   }		   
-	   }
-	   
-	   return successors;
-   }
+
+    /**
+     * Obtiene el peso entre el source y el destination
+     *
+     * @param source
+     * @param destination
+     * @return
+     */
+    public int getWeight(int source, int destination) {
+        return mat[source][destination];
+    }
+
+    /**
+     * Agrega un arco entre el source y el destination
+     *
+     * @param source
+     * @param destination
+     * @param weight
+     */
+    public void addArc(int source, int destination, int weight) {
+        mat[source][destination] = weight;
+    }
+
+    /**
+     * Retorna el sucesor del verticie
+     * @param vertex
+     * @return
+     */
+    public ArrayList<Integer> getSuccessors(int vertex) {
+        successors = new ArrayList<Integer>();
+        for (int i = 0; i < size(); i++) {
+            if (mat[vertex][i] != 0) {
+                successors.add(i);
+
+            }
+        }
+
+        return successors;
+    }
 }
