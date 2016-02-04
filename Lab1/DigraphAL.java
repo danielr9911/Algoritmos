7c39dd16e5fb4e947ab014a0b3f500d9b3884096
danielr9911 <dani-9911@hotmail.com>
2016-02-04 11:43:30 -0500

    Comentado clase AL

--- Lab1/DigraphAL.java
+++ Lab1/DigraphAL.java
@@ -1,101 +1,120 @@
 import java.util.ArrayList;
+
 /**
- * Esta clase es una implementación de un digrafo usando listas de adyacencia
- * 
- * @author  Mauricio Toro 
- * @author  Daniel Rendón Montaño
- * @author  Juan Daniel Morales Arias
- * @author  Cristyam Sepulveda Vasquez
+ * Esta clase es una implementaciï¿½n de un digrafo usando listas de adyacencia
+ *
+ * @author Mauricio Toro
+ * @author Daniel Rendï¿½n Montaï¿½o
+ * @author Juan Daniel Morales Arias
+ * @author Cristyam Sepulveda Vasquez
  * @version 04/02/2016
  */
-public class DigraphAL extends Graph
-{
+public class DigraphAL extends Graph {
     private ArrayList<ArrayList<Integer>> list;
     private ArrayList<Integer> successors;
-    
-   public DigraphAL(int size)
-   {
-     super(size);
-     list = new ArrayList<ArrayList<Integer>>();
-     
-   }
-   
-   public void addArc(int source, int destination, int weight)
-   {
-       ArrayList<Integer> arc = new ArrayList<Integer>();
-         
-       int i = 0;
-       if(list.size()!=0){    
-           while (list.get(i)!=null){
-               int num = list.get(i).get(0);
-               if (num==source){
-                   list.get(i).add(destination);
-                   list.get(i).add(weight);
-                   return;
-               }        
-               
-               i++;
-               if(i==list.size()){
-                break;
+
+
+    /**
+     * Constructor de la clase DiagraphAL
+     * @param size
+     */
+    public DigraphAL(int size) {
+        super(size);
+        list = new ArrayList<ArrayList<Integer>>();
+
+    }
+
+    /**
+     * AÃ±ade un nuevo arco entre el source y el
+     * destination con el correspondiente weight.
+     * @param source
+     * @param destination
+     * @param weight
+     */
+    public void addArc(int source, int destination, int weight) {
+        ArrayList<Integer> arc = new ArrayList<Integer>();
+
+        int i = 0;
+        if (list.size() != 0) {
+            while (list.get(i) != null) {
+                int num = list.get(i).get(0);
+                if (num == source) {
+                    list.get(i).add(destination);
+                    list.get(i).add(weight);
+                    return;
                 }
-               
-           }               
-        } 
-         arc.add(source);
-         arc.add(destination);
-         arc.add(weight);
-         list.add(arc);
-         return;
-     
-   }
-   
-   public int getWeight(int source, int destination)
-   {
-      int i = 0;
-      while(list.get(i)!=null){
-          
-          ArrayList<Integer> subList = list.get(i);
-          
-          if(subList.get(0)==source){
-            int size= subList.size();
-            for (int j=1;j<size;j++){
-                if(subList.get(j)==destination){
-                    return subList.get(j+1);
+
+                i++;
+                if (i == list.size()) {
+                    break;
                 }
-                
+
             }
-              
-          }
-          i++;
-          
-      }
-      return 0;
-      
-   }
-  
-   public ArrayList<Integer> getSuccessors(int vertice)
-   {
-       successors = new ArrayList<Integer>();
-       
-       int i = 0;
-          while(list.get(i)!=null){
-              
-              ArrayList<Integer> subList = list.get(i);
-              
-              if(subList.get(0)==vertice){
-                int size= subList.size();
-                for (int j=1;j<=size-2;j+=2){
+        }
+        arc.add(source);
+        arc.add(destination);
+        arc.add(weight);
+        list.add(arc);
+        return;
+
+    }
+
+    /**
+     * Obtiene el peso entre source y destination.
+     * @param source
+     * @param destination
+     * @return
+     */
+    public int getWeight(int source, int destination) {
+        int i = 0;
+        while (list.get(i) != null) {
+
+            ArrayList<Integer> subList = list.get(i);
+
+            if (subList.get(0) == source) {
+                int size = subList.size();
+                for (int j = 1; j < size; j++) {
+                    if (subList.get(j) == destination) {
+                        return subList.get(j + 1);
+                    }
+
+                }
+
+            }
+            i++;
+
+        }
+        return 0;
+
+    }
+
+    /**
+     * Obtiene los sucesores del vertice
+     * @param vertice
+     * @return
+     */
+    public ArrayList<Integer> getSuccessors(int vertice) {
+        successors = new ArrayList<Integer>();
+
+        int i = 0;
+        while (list.get(i) != null) {
+
+            ArrayList<Integer> subList = list.get(i);
+
+            if (subList.get(0) == vertice) {
+                int size = subList.size();
+                for (int j = 1; j <= size - 2; j += 2) {
                     successors.add(subList.get(j));
-                    
+
                 }
                 return successors;
-                  
-              }
-              i++;
-              
-          }
-                  
-          return successors;
-   
-   }
+
+            }
+            i++;
+
+        }
+
+        return successors;
+
+    }
 }
--- Lab1/DigraphAM.java
+++ Lab1/DigraphAM.java
@@ -1,50 +1,46 @@
 import java.util.ArrayList;
+
 /**
- * Esta clase es una implementación de un digrafo usando matrices de adyacencia
- * 
- * @author Mauricio Toro 
- * @author  Daniel Rendón Montaño
- * @author  Juan Daniel Morales Arias
- * @author  Cristyam Sepulveda Vasquez
+ * Esta clase es una implementaciï¿½n de un digrafo usando matrices de adyacencia
+ *
+ * @author Mauricio Toro
+ * @author Daniel Rendï¿½n Montaï¿½o
+ * @author Juan Daniel Morales Arias
+ * @author Cristyam Sepulveda Vasquez
  * @version 04/02/2016
  */
-public class DigraphAM extends Graph
-{
-	private int [][] mat;
-	private ArrayList<Integer> successors;
-   
-   /**
-    * Constructor de la clase DiagphAM
-    */
-	public DigraphAM(int size)
-   {
-	   super(size);
-       mat = new int [size][size];   
-   }
-   
-   /**
-    * 
-    */
-   public int getWeight(int source, int destination)
-   {
-	   return mat[source][destination];
+public class DigraphAM extends Graph {
+    private int[][] mat;
+    private ArrayList<Integer> successors;
+
+    /**
+     * Constructor de la clase DiagphAM
+     */
+    public DigraphAM(int size) {
+        super(size);
+        mat = new int[size][size];
     }
-   
-   public void addArc(int source, int destination, int weight)
-   {
-	   mat[source][destination] = weight;
-   }
-  
-   public ArrayList<Integer> getSuccessors(int vertex)
-   {
-	   successors = new ArrayList<Integer>();
-	   for(int i=0;i<size();i++){
-		   if(mat[vertex][i]!=0){
-			   successors.add(i);
-			   
-		   }		   
-	   }
-	   
-	   return successors;
-   }
+
+    /**
+     *
+     */
+    public int getWeight(int source, int destination) {
+        return mat[source][destination];
+    }
+
+    public void addArc(int source, int destination, int weight) {
+        mat[source][destination] = weight;
+    }
+
+    public ArrayList<Integer> getSuccessors(int vertex) {
+        successors = new ArrayList<Integer>();
+        for (int i = 0; i < size(); i++) {
+            if (mat[vertex][i] != 0) {
+                successors.add(i);
+
+            }
+        }
+
+        return successors;
+    }
 }
