2c4ef19ad5d9c704295b780ebad9e6925e5ebc4d
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2016-02-04 11:47:41 -0500

    clean

--- Lab1/DigraphAM.java
+++ Lab1/DigraphAM.java
@@ -1,17 +1,8 @@
 import java.util.ArrayList;
 
 /**
-<<<<<<< HEAD
- * Esta clase es una implementacion de un digrafo usando matrices de adyacencia
- *
  * @author Mauricio Toro
  * @author Daniel Rendon Montano
-=======
- * Esta clase es una implementaci�n de un digrafo usando matrices de adyacencia
- *
- * @author Mauricio Toro
- * @author Daniel Rend�n Monta�o
->>>>>>> origin/master
  * @author Juan Daniel Morales Arias
  * @author Cristyam Sepulveda Vasquez
  * @version 04/02/2016
@@ -29,21 +20,16 @@ public class DigraphAM extends Graph {
     }
 
     /**
-<<<<<<< HEAD
      * Obtiene el peso entre el source y el destination
      *
      * @param source
      * @param destination
      * @return
-=======
-     *
->>>>>>> origin/master
      */
     public int getWeight(int source, int destination) {
         return mat[source][destination];
     }
 
-<<<<<<< HEAD
     /**
      * Agrega un arco entre el source y el destination
      *
@@ -51,20 +37,15 @@ public class DigraphAM extends Graph {
      * @param destination
      * @param weight
      */
-=======
->>>>>>> origin/master
     public void addArc(int source, int destination, int weight) {
         mat[source][destination] = weight;
     }
 
-<<<<<<< HEAD
     /**
      * Retorna el sucesor del verticie
      * @param vertex
      * @return
      */
-=======
->>>>>>> origin/master
     public ArrayList<Integer> getSuccessors(int vertex) {
         successors = new ArrayList<Integer>();
         for (int i = 0; i < size(); i++) {
