01750e988c6efbf6f4106893e30ed76109b90cc2
danielr9911 <dani-9911@hotmail.com>
2016-04-02 15:51:35 -0500

    Cambios caminosGrafo

--- Laboratorio4/CaminosGrafo/src/CaminosGrafo.java
+++ Laboratorio4/CaminosGrafo/src/CaminosGrafo.java
@@ -1,5 +1,3 @@
- 
-
 import java.util.ArrayList;
 
 /**
@@ -7,19 +5,19 @@ import java.util.ArrayList;
  */
 public class CaminosGrafo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Graph grafo = new GraphAM(3);
-        grafo.addArc(0,0,1);
-        grafo.addArc(0,1,1);
-        grafo.addArc(0,2,1);
-        grafo.addArc(1,0,1);
-        grafo.addArc(1,1,1);
-        grafo.addArc(1,2,1);
-        grafo.addArc(2,0,1);
-        grafo.addArc(2,1,1);
-        grafo.addArc(2,2,1);
+        //grafo.addArc(0, 0, 1);
+        grafo.addArc(0, 1, 1);
+        grafo.addArc(0, 2, 1);
+        grafo.addArc(1, 0, 1);
+        //grafo.addArc(1, 1, 1);
+        grafo.addArc(1, 2, 1);
+        grafo.addArc(2, 0, 1);
+        grafo.addArc(2, 1, 1);
+        //grafo.addArc(2, 2, 1);
         ArrayList<ArrayList<Integer>> listaCaminos = caminos(grafo);
-        for(int i=0;i<listaCaminos.size();i++){
+        for (int i = 0; i < listaCaminos.size(); i++) {
             System.out.println(listaCaminos.get(i));
         }
 
@@ -29,21 +27,53 @@ public class CaminosGrafo {
         //System.out.prin tln(prueba);
     }
 
-    public static ArrayList<ArrayList<Integer>> caminos(Graph g){
+    public static ArrayList<ArrayList<Integer>> caminos(Graph g) {
+//        ArrayList<ArrayList<Integer>> listaCaminos = new ArrayList<ArrayList<Integer>>();
+//        ArrayList<Integer> camino1 = new ArrayList<>();
+//        ArrayList<Integer> camino2 = new ArrayList<>();
+//        camino1.add(0);
+//        camino1.add(1);
+//        camino1.add(2);
+//        camino2.add(1);
+//        camino2.add(3);
+//        listaCaminos.add(camino1);
+//        listaCaminos.add(camino2);
+//        return listaCaminos;
         ArrayList<ArrayList<Integer>> listaCaminos = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> camino1 = new ArrayList<>();
-        ArrayList<Integer> camino2 = new ArrayList<>();
-        camino1.add(0);
-        camino1.add(1);
-        camino1.add(2);
-        camino2.add(1);
-        camino2.add(3);
-        listaCaminos.add(camino1);
-        listaCaminos.add(camino2);
+
+        for (int i = 0; i < g.size(); i++) {
+            boolean[] visitados = new boolean[g.size()];
+            for (int j = 0; j < g.size(); j++) {
+                if(i!=j) {
+
+                    ArrayList<Integer> cam = caminosAux(g, i, j, visitados);
+                    listaCaminos.add(cam);
+                }
+            }
+        }
         return listaCaminos;
     }
 
-    public static void caminosAux(Graph g, boolean[] visitados){
-
+    public static ArrayList<Integer> caminosAux(Graph g, int p, int q, boolean[] visitados) {
+        visitados[p] = true;
+        ArrayList<Integer> sucesores = g.getSuccessors(p);
+        ArrayList<Integer> output = new ArrayList<>();
+        output.add(p);
+        for (int i = 0; i < sucesores.size(); i++) {
+            if (!visitados[sucesores.get(i)]) {
+                if (sucesores.get(i) == q) {
+                    output.add(sucesores.get(i));
+                    return output;
+                }
+                ArrayList<Integer> temp = caminosAux(g, sucesores.get(i), q, visitados);
+                if (temp.size() != 0) {
+                    output.addAll(temp);
+                    return output;
+                }
+            }
+        }
+        output = new ArrayList<Integer>();
+        return output;
     }
+
 }
